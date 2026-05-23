0249d041ed7844b32d89099a6a408bef019652f4
Mukti Gupta <152020110+mukti256@users.noreply.github.com>
2026-05-23 19:01:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
 app.get("/", (req, res) => {
-  res.send("API Running");
+  res.send("API Running Successfully");
 });
-
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/projects", require("./routes/projectRoutes"));
-app.use("/api/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 3000;
 
