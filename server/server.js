76b6c0d99efbfcfec8268899836436125e284946
Mukti Gupta <152020110+mukti256@users.noreply.github.com>
2026-05-23 18:09:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
